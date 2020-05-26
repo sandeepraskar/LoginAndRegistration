c28e38032d2595d69d32cb385d9bd2d3fa82c946
sandeepraskar <sandeepraskar@gmail.com>
2020-05-26 11:09:26 +0530

    Update Userform.java

--- app/src/main/java/com/example/loginandregistration/Userform.java
+++ app/src/main/java/com/example/loginandregistration/Userform.java
@@ -3,11 +3,15 @@ package com.example.loginandregistration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -16,16 +20,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Userform extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    Button _btnReg,btnGet;
+    Button _btnReg;
     EditText _txtName, _txtMName, _txtLName,_txtCaste,
             _txtOther,_txtDob,_txtMarStatus,_txtQualification,_txtPhone,_txtExpect,_txtHeight,
             _txtAdd, _txtEmail, _txtUser, _txtPass;
     Spinner _spinnerSubCaste;
-    TextView tvw;
-    DatePickerDialog picker;
+    private TextView mDisplayDate;
+    private static final String TAG = "Userform";
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +45,7 @@ public class Userform extends AppCompatActivity implements AdapterView.OnItemSel
         _txtCaste=(EditText)findViewById(R.id.txtCaste);
         _spinnerSubCaste=(Spinner) findViewById(R.id.spinnerSubCaste);
         _txtOther=(EditText)findViewById(R.id.txtOther);
-        _txtDob=(EditText)findViewById(R.id.txtDob);
+//        _txtDob=(EditText)findViewById(R.id.txtDob);
         _txtMarStatus=(EditText)findViewById(R.id.txtMarStatus);
         _txtQualification=(EditText)findViewById(R.id.txtQualification);
         _txtPhone=(EditText)findViewById(R.id.txtPhone);
@@ -49,6 +55,7 @@ public class Userform extends AppCompatActivity implements AdapterView.OnItemSel
         _txtUser=(EditText)findViewById(R.id.txtUser);
         _txtPass=(EditText)findViewById(R.id.txtPass);
         _txtAdd=(EditText) findViewById(R.id.txtAdd);
+
 
         Spinner spinSubCaste = (Spinner) findViewById(R.id.spinnerSubCaste);
         // Spinner click listener
@@ -68,34 +75,33 @@ public class Userform extends AppCompatActivity implements AdapterView.OnItemSel
         // attaching data adapter to spinner
         spinSubCaste.setAdapter(dataAdapters);
 
-//        tvw=(TextView)findViewById(R.id.textView1);
-//        eText=(EditText) findViewById(R.id.editText1);
-//        eText.setInputType(InputType.TYPE_NULL);
-//        eText.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                final Calendar cmdr = Calendar.getInstance();
-//                int day = cmdr.get(Calendar.DAY_OF_MONTH);
-//                int month = cmdr.get(Calendar.MONTH);
-//                int year = cmdr.get(Calendar.YEAR);
-//                // date picker dialog
-//                picker = new DatePickerDialog(userform.this,
-//                        new DatePickerDialog.OnDateSetListener() {
-//                            @Override
-//                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-//                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
-//                            }
-//                        }, year, month, day);
-//                picker.show();
-//            }
-//        });
-//        btnGet=(Button)findViewById(R.id.button1);
-//        btnGet.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                tvw.setText("Selected Date: "+ eText.getText());
-//            }
-//        });
+        mDisplayDate = (TextView) findViewById(R.id.txtDob);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        Userform.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: dd/mm/yyy: " + day + "/" + month + "/" + year);
+                String date = day + "/" + month + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
 
         setTitle("New User Registration");
         _btnReg.setOnClickListener(new View.OnClickListener() {
