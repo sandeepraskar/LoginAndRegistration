852f5bdc8f9c7eee58d84235599b9f8acb20c151
Sandeep <sandeepraskar@gmail.com>
2020-05-28 08:39:02 +0530

    Updated on 28/05/2020

--- app/src/main/java/com/example/loginandregistration/MainActivity.java
+++ app/src/main/java/com/example/loginandregistration/MainActivity.java
@@ -1,19 +1,11 @@
 package com.example.loginandregistration;
 
-import android.app.DatePickerDialog;
+
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TextView;
-
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     Button _btnReg, _btnLogin;
